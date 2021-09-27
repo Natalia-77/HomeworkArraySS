8d6b03c7ef500c5d6b1b0e907e809244624d19ce
vlasnata80@gmail.com <vlasnata80@gmail.com>
2021-09-28 00:34:46 +0300

    Arrays

---
+++ ArrayTasks/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ArrayTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            Console.WriteLine("Enter numbers for array: ");
+            for (int i = 0; i < 5; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("One-dimensional array:");
+            foreach (var item in array)
+            {
+                Console.Write($"{item}  ");
+            }
+            Console.WriteLine();
+
+            Random r = new Random();
+            int[,] mas = new int[4, 3];
+            Console.WriteLine("Two-dimensional array:");
+            for (int i = 0; i < 4; i++) // row
+            {
+                for (int j = 0; j <3; j++)//col
+                {
+                    mas[i, j] = r.Next(10,50); // 10-50
+                    Console.Write($"{mas[i, j]} ");
+                }
+                Console.WriteLine();
+               
+            }
+
+            
+
+        }
+    }
+}
